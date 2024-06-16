79feff18852c5d5abc243515e25ddc3626198274
darshitbhalodialampros <darshitbhalodia@lampros.tech>
2024-06-16 15:14:41 +0530

    unsupported list

--- apps/web/src/constants/lists.ts
+++ apps/web/src/constants/lists.ts
@@ -2,17 +2,17 @@ export const UNI_LIST = "https://tokenlist-api.vercel.app/v3-tokens";
 export const GRAPH_ENDPOINT =
   "https://api.goldsky.com/api/public/project_clvqb3g2poub601xzgkzc9oxs/subgraphs/udonswap-v3/1/gn"
 export const UNI_EXTENDED_LIST =
-  "https://cloudflare-ipfs.com/ipns/extendedtokens.uniswap.org";
+  "https://tokenlist-api.vercel.app/v3-tokens";
 const UNI_UNSUPPORTED_LIST =
-  "https://cloudflare-ipfs.com/ipns/unsupportedtokens.uniswap.org";
-const AAVE_LIST = "tokenlist.aave.eth";
-const BA_LIST =
-  "https://raw.githubusercontent.com/The-Blockchain-Association/sec-notice-list/master/ba-sec-list.json";
+  "https://unsupported-token-list.vercel.app/unsupported-tokens";
+// const AAVE_LIST = "tokenlist.aave.eth";
+// const BA_LIST =
+//   "https://raw.githubusercontent.com/The-Blockchain-Association/sec-notice-list/master/ba-sec-list.json";
 // TODO(WEB-2282): Re-enable CMC list once we have a better solution for handling large lists.
 // const CMC_ALL_LIST = 'https://s3.coinmarketcap.com/generated/dex/tokens/eth-tokens-all.json'
-const COINGECKO_LIST = "https://tokens.coingecko.com/uniswap/all.json";
-const COINGECKO_BNB_LIST =
-  "https://tokens.coingecko.com/binance-smart-chain/all.json";
+// const COINGECKO_LIST = "https://tokens.coingecko.com/uniswap/all.json";
+// const COINGECKO_BNB_LIST =
+//   "https://tokens.coingecko.com/binance-smart-chain/all.json";
 // const COINGECKO_ARBITRUM_LIST =
 //   "https://tokens.coingecko.com/arbitrum-one/all.json";
 // const COINGECKO_OPTIMISM_LIST =
@@ -20,13 +20,13 @@ const COINGECKO_BNB_LIST =
 // const COINGECKO_POLYGON_LIST =
 //   "https://tokens.coingecko.com/polygon-pos/all.json";
 // const COINGECKO_AVAX_LIST = "https://tokens.coingecko.com/avalanche/all.json";
-const COMPOUND_LIST =
-  "https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json";
-const GEMINI_LIST = "https://www.gemini.com/uniswap/manifest.json";
-const KLEROS_LIST = "t2crtokens.eth";
-const SET_LIST =
-  "https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json";
-const WRAPPED_LIST = "wrapped.tokensoft.eth";
+// const COMPOUND_LIST =
+//   "https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json";
+// const GEMINI_LIST = "https://www.gemini.com/uniswap/manifest.json";
+// const KLEROS_LIST = "t2crtokens.eth";
+// const SET_LIST =
+//   "https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json";
+// const WRAPPED_LIST = "wrapped.tokensoft.eth";
 
 // mode testnet token list api
 export const MODE_LIST = "https://tokenlist-api.vercel.app/v3-tokens";
@@ -40,25 +40,25 @@ export const MODE_LIST = "https://tokenlist-api.vercel.app/v3-tokens";
 // export const BASE_LIST =
 //   "https://raw.githubusercontent.com/ethereum-optimism/ethereum-optimism.github.io/master/optimism.tokenlist.json";
 
-export const UNSUPPORTED_LIST_URLS: string[] = [BA_LIST, UNI_UNSUPPORTED_LIST];
+export const UNSUPPORTED_LIST_URLS: string[] = [UNI_UNSUPPORTED_LIST];
 
 // default lists to be 'active' aka searched across
 export const DEFAULT_ACTIVE_LIST_URLS: string[] = [UNI_LIST];
 export const DEFAULT_INACTIVE_LIST_URLS: string[] = [
   UNI_EXTENDED_LIST,
-  COMPOUND_LIST,
-  AAVE_LIST,
+  // COMPOUND_LIST,
+  // AAVE_LIST,
   //  CMC_ALL_LIST,
-  COINGECKO_LIST,
-  COINGECKO_BNB_LIST,
+  // COINGECKO_LIST,
+  // COINGECKO_BNB_LIST,
   // COINGECKO_ARBITRUM_LIST,
   // COINGECKO_OPTIMISM_LIST,
   // COINGECKO_POLYGON_LIST,
   // COINGECKO_AVAX_LIST,
-  KLEROS_LIST,
-  GEMINI_LIST,
-  WRAPPED_LIST,
-  SET_LIST,
+  // KLEROS_LIST,
+  // GEMINI_LIST,
+  // WRAPPED_LIST,
+  // SET_LIST,
   // ARBITRUM_LIST,
   MODE_LIST,
   // OPTIMISM_LIST,
