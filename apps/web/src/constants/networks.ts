268b8b78f894dda026db4ac258f38213516b33c3
darshitbhalodialampros <darshitbhalodia@lampros.tech>
2024-06-16 15:23:33 +0530

    change explorer to rpc url for rpc

--- apps/web/src/constants/networks.ts
+++ apps/web/src/constants/networks.ts
@@ -47,7 +47,7 @@ export const PUBLIC_RPC_URLS: Record<SupportedInterfaceChain, string[]> = {
   //   "https://rpc.ankr.com/eth",
   //   "https://eth-mainnet.public.blastapi.io",
   // ],
-  [ChainId.MODE]: ["https://sepolia.explorer.mode.network/"],
+  [ChainId.MODE]: ["https://sepolia.mode.network/"],
   // [ChainId.GOERLI]: [
   //   // "Safe" URLs
   //   "https://rpc.goerli.mudit.blog/",
@@ -146,7 +146,7 @@ export const APP_RPC_URLS: Record<SupportedInterfaceChain, string[]> = {
   //   `https://mainnet.infura.io/v3/${INFURA_KEY}`,
   //   QUICKNODE_MAINNET_RPC_URL,
   // ],
-  [ChainId.MODE]: ["https://sepolia.explorer.mode.network/"],
+  [ChainId.MODE]: ["https://sepolia.mode.network/"],
   // [ChainId.GOERLI]: [`https://goerli.infura.io/v3/${INFURA_KEY}`],
   // [ChainId.SEPOLIA]: [`https://sepolia.infura.io/v3/${INFURA_KEY}`],
   // [ChainId.OPTIMISM]: [`https://optimism-mainnet.infura.io/v3/${INFURA_KEY}`],
